ec26271d0b550cea9a41bd7b9c1edf00d8543061
Deepanshuchauhan123 <deepanshuchauhan7287@gmail.com>
2020-05-12 14:52:16 +0530

    Task Added

--- lib/CreateTask.dart
+++ lib/CreateTask.dart
@@ -2,16 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
 
-// ...
-
 class CreateTask extends StatefulWidget {
   @override
   _CreateTaskState createState() => _CreateTaskState();
 }
 
 class _CreateTaskState extends State<CreateTask> {
-  final format = DateFormat("yyyy-MM-dd");
+  TimeOfDay _time = TimeOfDay.now();
+  TimeOfDay picked;
   DateTime _dateTime;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,147 +24,254 @@ class _CreateTaskState extends State<CreateTask> {
             children: <Widget>[
               Image.asset(
                 'assets/images/addtask.gif',
-                height: 250,
+                height: 200,
                 width: double.maxFinite,
               ),
-              Padding(
-                padding: EdgeInsets.fromLTRB(20, 20, 20, 10),
-                child: Container(
-                  padding: EdgeInsets.all(5),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.blueGrey[200],
-                        blurRadius: 15.0,
-                        offset: Offset(
-                          0,
-                          10,
-                        ),
-                      ),
-                    ],
-                  ),
+              Expanded(
+                child: SingleChildScrollView(
                   child: Container(
-                    padding: EdgeInsets.fromLTRB(5, 5, 5, 0),
-                    decoration: BoxDecoration(
-                      border: Border(
-                        bottom: BorderSide(
-                          color: Colors.black,
+                    child: Column(
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(20, 20, 20, 10),
+                          child: Container(
+                            padding: EdgeInsets.all(5),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.blueGrey[200],
+                                  blurRadius: 15.0,
+                                  offset: Offset(
+                                    0,
+                                    10,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            child: Container(
+                              padding: EdgeInsets.fromLTRB(5, 5, 5, 0),
+                              decoration: BoxDecoration(
+                                border: Border(
+                                  bottom: BorderSide(
+                                    color: Colors.black,
+                                  ),
+                                ),
+                              ),
+                              child: TextField(
+                                decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  hintText: "Title",
+                                  hintStyle: TextStyle(
+                                    color: Colors.grey,
+                                    fontSize: 18,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
                         ),
-                      ),
-                    ),
-                    child: TextField(
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: "Title",
-                        hintStyle: TextStyle(
-                          color: Colors.grey,
-                          fontSize: 18,
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                          child: Container(
+                            padding: EdgeInsets.fromLTRB(0, 10, 0, 5),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.blueGrey[200],
+                                  blurRadius: 15.0,
+                                  offset: Offset(
+                                    0,
+                                    10,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            child: FlatButton(
+                              onPressed: () {
+                                showDatePicker(
+                                        context: context,
+                                        initialDate: _dateTime == null
+                                            ? DateTime.now()
+                                            : _dateTime,
+                                        firstDate: DateTime(2020),
+                                        lastDate: DateTime(2025))
+                                    .then(
+                                  (date) {
+                                    setState(
+                                      () {
+                                        _dateTime = date;
+                                      },
+                                    );
+                                  },
+                                );
+                              },
+                              child: Container(
+                                height: 45,
+                                width: double.maxFinite,
+                                padding: EdgeInsets.fromLTRB(0, 5, 5, 0),
+                                decoration: BoxDecoration(
+                                  border: Border(
+                                    bottom: BorderSide(
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                ),
+                                child: Text(
+                                  _dateTime == null
+                                      ? "Pick a Date"
+                                      : _dateTime.toString().substring(0, 10),
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 17,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
                         ),
-                      ),
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                          child: Container(
+                            padding: EdgeInsets.fromLTRB(0, 10, 0, 5),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.blueGrey[200],
+                                  blurRadius: 15.0,
+                                  offset: Offset(
+                                    0,
+                                    10,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            child: FlatButton(
+                              onPressed: () {
+                                showTimePicker(
+                                  context: context,
+                                  initialTime: _time,
+                                ).then(
+                                  (time) {
+                                    setState(
+                                      () {
+                                        picked = time;
+                                      },
+                                    );
+                                  },
+                                );
+                              },
+                              child: Container(
+                                height: 45,
+                                width: double.maxFinite,
+                                padding: EdgeInsets.fromLTRB(0, 5, 5, 0),
+                                decoration: BoxDecoration(
+                                  border: Border(
+                                    bottom: BorderSide(
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                ),
+                                child: Text(
+                                  picked == null
+                                      ? "Task Start Time"
+                                      : picked.format(context).toString(),
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 17,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(20, 20, 20, 10),
+                          child: Container(
+                            padding: EdgeInsets.all(5),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.blueGrey[200],
+                                  blurRadius: 15.0,
+                                  offset: Offset(
+                                    0,
+                                    10,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            child: Container(
+                              padding: EdgeInsets.fromLTRB(5, 5, 5, 0),
+                              decoration: BoxDecoration(
+                                border: Border(
+                                  bottom: BorderSide(
+                                    color: Colors.black,
+                                  ),
+                                ),
+                              ),
+                              child: TextField(
+                                decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  hintText: "Description",
+                                  hintStyle: TextStyle(
+                                    color: Colors.grey,
+                                    fontSize: 18,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 40,
+                        ),
+                        FlatButton(
+                          onPressed: () {},
+                          child: Padding(
+                            padding: const EdgeInsets.all(20.0),
+                            child: Container(
+                              height: 50,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(
+                                  10,
+                                ),
+                                gradient: LinearGradient(
+                                  colors: [
+                                    Color.fromRGBO(143, 148, 251, 1),
+                                    Color.fromRGBO(143, 148, 251, .6),
+                                  ],
+                                ),
+                              ),
+                              child: Center(
+                                child: Text(
+                                  "Add Task",
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                 ),
-              ),
-
-
-
-
-
-
-              Padding(
-                padding: EdgeInsets.fromLTRB(20, 20, 20, 10),
-                child: Container(
-                  padding: EdgeInsets.all(5),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.blueGrey[200],
-                        blurRadius: 15.0,
-                        offset: Offset(
-                          0,
-                          10,
-                        ),
-                      ),
-                    ],
-                  ),
-                  child: Container(
-                    height: 45,
-                    width: double.maxFinite,
-                    padding: EdgeInsets.fromLTRB(5, 5, 5, 0),
-                    decoration: BoxDecoration(
-                      border: Border(
-                        bottom: BorderSide(
-                          color: Colors.black,
-                        ),
-                      ),
-                    ),
-                    margin: EdgeInsets.fromLTRB(0, 8, 40, 0),
-                    child: Text(
-                    _dateTime == null ? "Pick a Date" : _dateTime.toString(),
-                    style: TextStyle(color: Colors.black,fontSize: 17,),
-                  ),
-                    
-                  ),
-                ),
-              ),
-
-
-
-
-
-
-              Column(
-                children: <Widget>[
-                  Text(
-                    _dateTime == null ? "Pick a Date" : _dateTime.toString(),
-                  ),
-                  RaisedButton(
-                    child: Text("Date"),
-                    onPressed: (){
-                      showDatePicker(
-                        context: context,
-                        initialDate: _dateTime == null ? DateTime.now():
-                        _dateTime,
-                        firstDate: DateTime(2020),
-                        lastDate: DateTime(2025)
-                      ).then((date){
-                        setState(() {
-                          _dateTime=date;
-                        });
-                      });
-                    },
-                  )
-                ],
               )
             ],
           ),
         ),
       ),
     );
-  }
-}
-
-class BasicDateField extends StatelessWidget {
-  final format = DateFormat("yyyy-MM-dd");
-  DateTime _dateTime;
-  @override
-  Widget build(BuildContext context) {
-    return Column(children: <Widget>[
-      Text('Basic date field (${format.pattern})'),
-      DateTimeField(
-        format: format,
-        onShowPicker: (context, currentValue) {
-          return showDatePicker(
-              context: context,
-              firstDate: DateTime(1900),
-              initialDate: currentValue ?? DateTime.now(),
-              lastDate: DateTime(2100));
-        },
-      ),
-    ]);
   }
 }
